a63af8f0a4cdbded3f4cdfa7751543b5f8ba495f
Pierog <steve.pierog@gmail.com>
2026-04-16 12:33:21 -0400

    Initial Commit

---
+++ FieldLog/FieldLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  FieldLogApp.swift
+//  FieldLog
+//
+//  Created by Steve Pierog on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct FieldLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
